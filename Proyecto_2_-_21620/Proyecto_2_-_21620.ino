cf9ae0d361c5eac5544207b6df753eba7913683d
aler21620 <rod21620@uvg.edu.gt>
2023-10-28 21:51:37 -0600

    Update Proyecto_2_-_21620.ino

--- Proyecto_2_-_21620/Proyecto_2_-_21620.ino
+++ Proyecto_2_-_21620/Proyecto_2_-_21620.ino
@@ -6,6 +6,24 @@
 //*****************************************************************************
 // Librerías
 //*****************************************************************************
+#include <stdint.h>
+#include <stdbool.h>
+#include <TM4C123GH6PM.h>
+#include <SPI.h>
+#include "inc/hw_ints.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "driverlib/debug.h"
+#include "driverlib/gpio.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/rom_map.h"
+#include "driverlib/rom.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/timer.h"
+
+#include "bitmaps.h"
+#include "font.h"
+#include "lcd_registers.h"
 
 //*****************************************************************************
 //  Definición de pines
